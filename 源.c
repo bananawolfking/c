ecbb4d353bb163b47a73eb2130f051df982482d4
蕉太狼 <128605083+bananawolfking@users.noreply.github.com>
2023-04-04 22:42:17 +0800

    Add files via upload

--- 源.c
+++ 源.c
@@ -1,142 +1,45 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-//#include<stdio.h>
-//
-//int pit(int n)
-//{
-//	int a = 1, b = 1, c = 1;
-//	while (n > 2)
-//	{
-//		c = a + b;
-//		a = b;
-//		b = c;
-//		n--;
-//	}
-//	return c;
-//}
-//
-//int main()
-//{
-//	int n = 0;
-//	int rat = 0;
-//	scanf("%d", &n);
-//	rat = pit(n);
-//	printf("%d", rat);
-//	return 0;
-//}
-
-
-//#include<stdio.h>//韩罗塔问题
-//
-//int Hanoi(int n)
-//{
-//	if (n > 1)
-//	{
-//		return 2 * Hanoi(n - 1)+1;
-//	}
-//	else
-//		return 1;
-//}
-//int Hanoi1(int m)
-//{
-//	int x = 1;
-//	while (m > 1)
-//	{
-//		x = 2 * x + 1;
-//		m--;
-//	}
-//	return x;
-//}
-//int main()
-//{
-//	int a = 0;
-//	scanf("%d", &a);
-//	int ret = Hanoi1(a);
-//	printf("%d", ret);
-//	return 0;
-//}
-
-
-//
-//#include<stdio.h>//青蛙跳台阶问题
-//
-//int Frog(int n)
-//{
-//	if (n > 2)
-//	{
-//		return Frog(n - 1) + Frog(n - 2);
-//	}
-//	else
-//		return 3;
-//}
-//int main()
-//{
-//	int a = 0;
-//	int ret = 0;
-//	scanf("%d", &a);
-//	ret = Frog(a);
-//	printf("%d", ret);
-//	return 0;
-//}
-
-
-
 #include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+
+void bubble(int* arr, int sz)
+{
+	int i = 0;
+	int j = 0;
+	int team = 0;
+	int flag = 0;
+	for (i = 0; i < sz - 1; i++)
+	{
+		j = 0;
+		flag = 0;
+		for (j = 0; j < sz - 1 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				team = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = team;
+				flag = 1;
+			}
+			
+		}
+		if (flag == 0)
+		{
+			break;
+		}
+	}
+}
+
+
 
 int main()
 {
-	int i = 0;
 	int x = 0;
-	int pen;
-	int a = 1;
-	int b = 1;
-	int j = 1;
-	int* p = (int*)calloc(10, sizeof(int));
-	if (p == NULL)
+	int arr[] = { 1,2,3,4,5,6,7,8,9 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	bubble(arr, sz);
+	for (x = 0; x < sz; x++)
 	{
-		printf("out of memory");
-		exit(EXIT_FAILURE);
+		printf("%d", arr[x]);
 	}
-	p[0] = 2;
-	for (i = 3; i < 200; i += 2)
-	{
-		x = 0;
-		x = 0;
-		while (p[x]>0 && p[x]<=sqrt(i))
-		{
-			pen = i % p[x];
-			if (pen == 0)
-				break;
-			x++;
-		}
-		if (!p[x] || p[x] > sqrt(i))
-		{
-			a++;
-			if (a >= 10 * b)
-			{
-				b++;
-				int* y = (int*)realloc(p, 10 * sizeof(int) * b);
-				if (y == NULL)
-				{
-					printf("out of memory");
-					exit(EXIT_FAILURE);
-				}
-				else
-				{
-					p = y;
-				}
-			}
-			p[j] = i;
-			j++;
-			printf("%d   ", i);
-		}
-	}
-
-
-
-
-
-
 	return 0;
 }
